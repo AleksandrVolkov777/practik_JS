bceabf1cd38db3ec810095d795826b5dbea035f1
Aleksandr <volk77793@gmail.com>
2021-01-25 22:40:41 +0300

    + cikl while and do while

--- app.js
+++ app.js
@@ -10,20 +10,52 @@ const appData = {
     savings: false
 };
 
-
+//let i = 0;
 
 for (let i = 0; i < 2; i++) {
     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
-        b = prompt('Во сколько обойдется?', '');
+        b = prompt('Во сколько обойдется?', '')
     
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null
-        && a != '' && b != '' && a.length < 50) {
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
+        a != '' && b != '' && a.length < 50) {
         console.log('done');
         appData.expenses[a] = b;
-    } else { 
-
-    }  
+    } else {
+        console.log('bad result');
+        i--;
+    }
 };
+
+// while ( i < 2 ) {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = prompt('Во сколько обойдется?', '');
+    
+//     if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null
+//         && a != '' && b != '' && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else { 
+//         console.log('bad result');
+//         i--;
+//     }  
+//     i++;
+// };
+
+// do  {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = prompt('Во сколько обойдется?', '');
+    
+//     if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null
+//         && a != '' && b != '' && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else { 
+//         console.log('bad result');
+//         i--;
+//     }  
+//     i++;
+// }
+// while (i < 2);
 
 appData.moneyPerDay = appData.budget / 30;
 
